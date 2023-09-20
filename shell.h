3209feefa298a0a572e2f880dd0d84108b1f6cee
Hudaelmahdy <hudaemad251@gmail.com>
2023-09-21 00:33:58 +0300

    done

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,60 +10,26 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
-#include <stddef.h>
-#include <signal.h>
-
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *string);
-char *_strdup(char *str);
-char *concat(char *name, char *sep, char *value);
-char **tokenizeString(char *str, const char *separator);
-void execute(char **args, char **envarray);
-void *re_alloc(void *oldmem, unsigned int currsize, unsigned int reqsize);
-
 
 extern char **envarray;
 
-/**
- * struct ls_path - link the list that containing path of directories
- * @directory: the directory in the path
- * @next_dir: pointer to the next directory
-*/
+char *extract_path(char **envp);
+int evaluate_path(char **args, char **envp);
+char *user_command(void);
+void get_env(char **envp);
+char **get_token(char *string);
+void exit_shell(char **args, char *string, int _exit);
+void *str_tok(char *str, const char *separator);
+int fork_function(char **args, char **argv, char **envp,
+char *string, int nproc, int cnt);
 
-typedef struct ls_path
-{
-	char *directory;
-	struct ls_path *next_dir;
-} ls_path;
+int str_cmp(char *s1, char *s2);
+size_t str_cmp(char *s1, char *s2, size_t n);
+int str_len(char *s);
+char *str_cpy(char *dest, char *src);
+char *str_cat(char *dest, char *src);
 
-
-char *get_env(const char *name, char **envarray);
-ls_path *push_node(ls_path **firstNode, char *str);
-ls_path *pathlink(char *pathstr);
-char *locate_command(char *command_name, ls_path *firstNode);
-
-/**
- * struct builtin - function to the pointer with corresponding builtin command
- * @name: builtin name.
- * @func: execute builtin command
-*/
-typedef struct builtin
-{
-	char *name;
-	void (*func)(char **, char **);
-} builtin;
-
-
-void (*lookup_builtin(char **argv, char **envarray))(char **, char **);
-int _atoi(char *string);
-void exit_shell(char **argv, char **envarray);
-void print_env(char **argv, char **envarray);
-void set_env(char **argv, char **envarray);
-void unset_env(char **argv, char **envarray);
-
-void freeargv(char **argv);
-void free_ls(ls_path *firstNode);
+int _putchar(char c);
 
 
 #endif
