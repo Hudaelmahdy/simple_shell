01dd4638026420ffd3fe4e4b6af30fbf3f2efb26
ChristinMelad <christinmelad@yahoo.com>
2023-09-21 01:21:25 +0300

    updates

--- Functions.c
+++ Functions.c
@@ -3,157 +3,55 @@
 /**
 *exit_shell - function to exit shell
 *@argv: array
+*@string: string
+*@_exit: value
+*Return: nothing
 */
 
-void exit_shell(char **argv, char **envarray)
+void exit_shell(char **argv, char *string, int _exit)
 {
-	int i, n;
-
-	(void)envarray;
+	int status = 0;
 
 	if (argv[1])
 	{
-		n = _atoi(argv[1]);
-			if (n <= -1)
-				n = 2;
-			freeargv(argv);
-			exit(n);
+		free(string);
+		free(argv);
+		exit(_exit);
 	}
-	for (i = 0; argv[i]; i++)
-		free(argv[i]);
+	status = atoi(argv[1]);
+
+	free(string);
 	free(argv);
-	exit(0);
+	exit(status);
 }
 
 /**
-*_atoi - convert string to integer
-*@string: pointer to string
+*extract_path - function to retrieve the value of the path env
+*@envarray: pointer to array
 *Return: integer
 */
 
-int _atoi(char *string)
+char extract_path(char **envarray)
 {
-	int i, integer, sign = 1;
+	size_t envIndex = 0, index = 0, pathLen = 5;
 
-	i = 0;
+	char *path = NULL;
 
-	integer = 0;
-	while (!((string[i] >= '0') && (string[i] <= '9')) && (string[i] != '\0'))
-	{
-		if (string[i] == '-')
-		{
-			sign = sign * (-1);
-		}
-		i++;
-	}
-	while ((string[i] >= '0') && (string[i] <= '9'))
-	{
-		integer = (integer * 10) + (sign * (string[i] - '0'));
-		i++;
-	}
-	return (integer);
-}
+	for (envIndex = 0; str_cmp(envarray[envIndex], "PATH=", 5); envIndex++)
+		;
+	if (envarray[envIndex] == NULL)
+	return (NULL);
 
-/**
-*print_env - prints the environment
-*envarray - store the value printed by the function
-*@argv: arguments
-*/
+	for (pathLen = 5; envarray[envIndex][index]; index++, pathLen++)
+		;
+	path = malloc(sizeof(char) * (pathLen + 1));
 
+	if (path == NULL)
+		return (NULL);
 
-void print_env(char **argv __attribute__ ((unused)), char **envarray)
-{
-	int i;
+	for (index = 5, pathLen = 0; envarray[envIndex][index]; index++, pathLen++)
+		path[pathLen] = envarray[envIndex][index];
 
-	for (i = 0; envarray[i]; i++)
-	{
-		_puts(envarray[i]);
-		_puts("\n");
-	}
-}
-
-/**
-*set_env - set new enviroment
-*@argv: arguments
-*/
-
-void set_env(char **argv, char **envarray)
-{
-	int i, j, k;
-
-	if (!argv[1] || !argv[2])
-	{
-		perror(get_env("_", envarray));
-		return;
-	}
-	for (i = 0; envarray[i]; i++)
-	{
-		j = 0;
-		if (argv[1][j] == envarray[i][j])
-		{
-			while (argv[1][j])
-			{
-				if (argv[1][j] != envarray[i][j])
-					break;
-				j++;
-			}
-			if (argv[i][j] == '\0')
-			{
-				k = 0;
-				while (argv[2][k])
-				{
-					envarray[i][j + 1 + k] = argv[2][k];
-					k++;
-				}
-				envarray[i][j + 1 + k] = '\0';
-				return;
-			}
-		}
-	}
-if (!envarray[i])
-	{
-	envarray[i] = concat(argv[1], "=", argv[2]);
-	envarray[i + 1] = '\0';
-	}
-}
-
-
-/**
-*unset_env - remove an enviroment
-*@argv: arguments
-*/
-
-void unset_env(char **argv, char **envarray)
-{
-	int i, j;
-
-	if (!argv[1])
-	{
-		perror(get_env("_", envarray));
-		return;
-	}
-	for (i = 0; envarray[i]; i++)
-	{
-		j = 0;
-		if (argv[1][j] == envarray[i][j])
-		{
-			while (argv[1][j])
-			{
-				if (argv[1][j] != envarray[i][j])
-					break;
-				j++;
-			}
-			if (argv[1][j] == '\0')
-			{
-				free(envarray[i]);
-				envarray[i] = envarray[i + 1];
-				while (envarray[i])
-				{
-					envarray[i] = envarray[i + 1];
-					i++;
-				}
-				return;
-			}
-		}
-	}
+	path[pathLen] = '\0';
+	return (path);
 }
