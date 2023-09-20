81ba8a6a756f5e605639b0fcf02f8162ccf528aa
ChristinMelad <christinmelad@yahoo.com>
2023-09-21 01:50:34 +0300

    updates

--- shell.h
+++ shell.h
@@ -11,20 +11,20 @@
 #include <sys/types.h>
 #include <errno.h>
 
-extern char **envarray;
+extern char **environ;
 
-char *extract_path(char **envp);
-int evaluate_path(char **args, char **envp);
+char *extract_path(char **envarray);
+int evaluate_path(char **argv, char **envarray);
 char *user_command(void);
-void get_env(char **envp);
+void get_env(char **envarray);
 char **get_token(char *string);
-void exit_shell(char **args, char *string, int _exit);
+void exit_shell(char **argv, char *string, int _exit);
 char *_strtok(char *str, const char *separator);
-int fork_function(char **args, char **argv, char **envp,
+int fork_function(char **argv, char **args, char **envarray,
 char *string, int nproc, int cnt);
 
 int str_cmp(char *s1, char *s2);
-size_t str_cmp(char *s1, char *s2, size_t n);
+size_t strn_cmp(char *s1, char *s2, size_t n);
 int str_len(char *s);
 char *str_cpy(char *dest, char *src);
 char *str_cat(char *dest, char *src);
